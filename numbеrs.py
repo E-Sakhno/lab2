45ae198d3127f534a0df9572936b9b06d32c12d1
E-Sakhno <miss.saxno@yandex.ru>
2020-10-31 03:22:02 +0300

    The commit 31.10.2020

---
+++ numbеrs.py
@@ -0,0 +1,9 @@
+value_1=int(input("Input the first value: "))
+value_2=int(input("Input the second value: "))
+value_3=int(input("Input the third value: "))
+value_4=int(input("Input the fourth value: "))
+
+sum_1_2=value_1+value_2
+sum_3_4=value_3+value_4
+divis=sum_1_2/sum_3_4
+print("Result = %.2f"%divis)
